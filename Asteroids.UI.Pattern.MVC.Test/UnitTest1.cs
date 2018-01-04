1f4e944a4ce85c783fbb840564d9cd4c62810682
Martin Korneffel <Martin.Korneffel@mk-prg-net.de>
2018-01-04 20:31:50 +0100

    Vorbereitung JAvaScript

---
+++ Asteroids.UI.Pattern.MVC.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MkPrgNet.Pattern.Repository;
+using AsteroidsBL;
+using AsteroidsDAL;
+using AsteroidsDAL.CSV;
+using Asteroids.UI.Pattern.MVC;
+
+namespace Asteroids.UI.Pattern.MVC.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //var cntrl = global::Asteroids.UI.Pattern.MVC.
+            //Asteroids.UI.Pattern.MVC
+
+        }
+    }
+}
